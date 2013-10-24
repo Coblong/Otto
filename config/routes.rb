9f5b9baee44679fa757e1cd8efa6a1f071afaba5
Paul Cobley <paul.cobley@gmail.com>
2013-10-24 23:03:12 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Otto::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  
+  root  'static_pages#home'
+  match '/', to: 'static_pages#home', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',  to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
