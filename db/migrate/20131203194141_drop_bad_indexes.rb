6e33f3a9a2bce03c02516d42e83e2be107c59e19
Paul Cobley <paul.cobley@gmail.com>
2014-01-05 20:16:20 +0000

    Remove shitty patch

--- db/migrate/20131203194141_drop_bad_indexes.rb
+++
@@ -1,7 +0,0 @@
-class DropBadIndexes < ActiveRecord::Migration
-  def change
-    remove_index :properties, name: :index_properties_on_agent_id_and_created_at
-    remove_index :properties, name: :index_properties_on_branch_id_and_created_at
-    remove_index :properties, name: :index_properties_on_estate_agent_id_and_created_at
-  end
-end
