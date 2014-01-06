703fbe1de37da323b1eda3a0f40ae56e5f9d1bc9
Paul Cobley <paul.cobley@gmail.com>
2014-01-06 16:39:26 +0000

    trying to fix the fucking images

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Otto::Application.config.session_store :cookie_store, key: '_Otto_session'
+Otto::Application.config.session_store :cookie_store, key: '_Ottor_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
