cebb49a974c110a83215b970568fa8ba0e2dd3f8
Paul Cobley <paul.cobley@gmail.com>
2014-01-06 17:32:59 +0000

    Tryng again without https

--- app/models/property.rb
+++ app/models/property.rb
@@ -10,7 +10,7 @@ class Property < ActiveRecord::Base
   default_scope -> { order('call_date', 'branch_id', 'created_at') }  
 
   def full_url
-    'https://' + url + external_ref
+    '//' + url + external_ref
   end
 
   def statuses
