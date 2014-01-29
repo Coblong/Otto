9f069ef187c091a43ad49ee8eac95ab584d99995
Paul Cobley <paul.cobley@gmail.com>
2014-01-29 13:32:34 +0000

    Dont filter viewings

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -99,7 +99,6 @@ module StaticPagesHelper
   def add_viewings
     @days_properties_hash = Hash.new
     properties = current_user.properties.where("view_date >= ?", Date.today.beginning_of_day).where(closed: false).order(:view_date)
-    properties = apply_filters(properties)
     agents = Branch.where("id in (?)", properties.collect(&:branch_id))
     @days_properties_hash[get_key('Viewings', properties.size, agents.size)] = properties
   end
